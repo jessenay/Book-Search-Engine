932c0db5b5c6cef5df1e070ee8bc7676652017ad
Jesse Nay <jessenay26@gmail.com>
2024-03-21 14:10:39 -0600

    added code to app.jsx and searchbooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,26 @@
+import React from 'react';
 import './App.css';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3001/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
